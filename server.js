797c8c83fece5c84f4a8af536f41654dc76b808d
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-08-07 09:21:10 -0500

    🐛 FIX: cors issue

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
-const pga = require('./pga/pga-router')
+const pga = require("./pga/pga-router");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(helmet());
 
-app.use('/api/pga', pga)
+app.use("/api/pga", pga);
 
 // --> /hello <-- //
 app.get("/hello", (req, res) => {
