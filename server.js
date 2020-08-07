980916099e31351d7ce1ef39f4e9c2b1edd95dcf
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-08-07 07:56:07 -0500

    🛠 SET UP: pga-router to handle 'api/pga'

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const pga = require('./pga/pga-router')
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(helmet());
+
+app.use('/api/pga', pga)
 
 // --> /hello <-- //
 app.get("/hello", (req, res) => {
